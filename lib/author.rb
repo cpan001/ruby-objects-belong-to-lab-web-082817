c7ea67fa4973cbe10757e4ea846927290dacdff9
cpan001 <github email address>
2017-08-31 13:56:28 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+class Author
+  attr_accessor :name
+
+end
+
+class Post
+  attr_accessor :title, :author
+
+end
+
+
+# binding.pry
+#
+# ""
--- lib/song.rb
+++ lib/song.rb
@@ -10,5 +10,5 @@ end
 
 
 # binding.pry
-#
+# 
 # ""
